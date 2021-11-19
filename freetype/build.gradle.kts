2da80005ee4f3fc1169c4b9c18b5745a7b5e1dac
Jakub Janků <janku.jakub.jj@gmail.com>
2021-11-19 15:06:30 +0100

    add freetype

---
+++ freetype/build.gradle.kts
@@ -0,0 +1,33 @@
+import com.android.ndkports.AndroidExecutableTestTask
+import com.android.ndkports.CMakeCompatibleVersion
+import com.android.ndkports.CMakePortTask
+
+val portVersion = "2.11.0"
+
+group = "com.android.ndk.thirdparty"
+version = "$portVersion${rootProject.extra.get("snapshotSuffix")}"
+
+plugins {
+    id("com.android.ndkports.NdkPorts")
+}
+
+ndkPorts {
+    ndkPath.set(File(project.findProperty("ndkPath") as String))
+    source.set(project.file("freetype-$portVersion.tar.gz"))
+    minSdkVersion.set(16)
+}
+
+val buildTask = tasks.register<CMakePortTask>("buildPort") {
+    cmake {
+        arg("-DBUILD_SHARED_LIBS=true")
+    }
+}
+
+tasks.prefabPackage {
+    version.set(CMakeCompatibleVersion.parse(portVersion))
+    licensePath.set("LICENSE.TXT")
+
+    modules {
+        create("freetype")
+    }
+}
